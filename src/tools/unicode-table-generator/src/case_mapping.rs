b6207dcce4e9f5fb96f2023d25b9eedad9e1e661
Karl Meakin <karl.meakin@arm.com>
2025-07-27 02:07:26 +0100

    split tables by unicode plane

--- src/tools/unicode-table-generator/src/case_mapping.rs
+++ src/tools/unicode-table-generator/src/case_mapping.rs
@@ -3,13 +3,9 @@ use std::fmt::{self, Write};
 
 use crate::{CaseMap, UnicodeData, fmt_list};
 
-const INDEX_MASK: u32 = 1 << 22;
-
 pub(crate) fn generate_case_mapping(data: &UnicodeData) -> Result<String, fmt::Error> {
     let mut file = String::new();
 
-    write!(file, "const INDEX_MASK: u32 = 0x{INDEX_MASK:x};")?;
-    file.push_str("\n\n");
     file.push_str(HEADER.trim_start());
     file.push('\n');
     file.push_str(&generate_tables("LOWER", &data.to_lower)?);
@@ -18,52 +14,75 @@ pub(crate) fn generate_case_mapping(data: &UnicodeData) -> Result<String, fmt::E
     Ok(file)
 }
 
-fn generate_tables(case: &str, data: &CaseMap) -> Result<String, fmt::Error> {
-    let mut mappings = Vec::with_capacity(data.len());
-    let mut multis = Vec::new();
+#[derive(Default, Clone)]
+struct Plane {
+    single_keys: Vec<HexEscape>,
+    single_vals: Vec<HexEscape>,
+    multi_keys: Vec<HexEscape>,
+    multi_vals: Vec<[CharEscape; 3]>,
+}
 
+fn decompose(c: char) -> (u16, u16) {
+    let plane = (c as u32) / 0x10_000;
+    let code = (c as u32) % 0x10_000;
+    assert!(plane <= 16);
+    (plane as u16, code as u16)
+}
+
+fn generate_tables(case: &str, data: &CaseMap) -> Result<String, fmt::Error> {
+    let mut planes = vec![Plane::default(); 17];
+    let mut num_planes = 0;
     for (&key, mapped) in data.iter() {
         if key.is_ascii() {
             continue;
         }
 
-        let value = match mapped {
-            &[a, '\0', '\0'] => u32::from(a),
-            &[a, b, c] => {
-                multis.push([CharEscape(a), CharEscape(b), CharEscape(c)]);
-                INDEX_MASK | (u32::try_from(multis.len()).unwrap() - 1)
+        let (key_plane, key_code) = decompose(key);
+        let plane = &mut planes[key_plane as usize];
+        match mapped {
+            &[val, '\0', '\0'] => {
+                let (val_plane, val_code) = decompose(val);
+                assert_eq!(key_plane, val_plane);
+                plane.single_keys.push(HexEscape(key_code));
+                plane.single_vals.push(HexEscape(val_code));
             }
-        };
+            &chars => {
+                plane.multi_keys.push(HexEscape(key_code));
+                plane.multi_vals.push(chars.map(CharEscape));
+            }
+        }
 
-        mappings.push((CharEscape(key), value));
+        num_planes = num_planes.max(key_plane + 1);
     }
+    planes.truncate(num_planes as usize);
 
     let mut tables = String::new();
-
-    let (keys, vals): (Vec<_>, Vec<_>) = mappings.into_iter().unzip();
-
-    writeln!(
-        tables,
-        "static {case}CASE_TABLE_KEYS: &[char; {}] = &[{}];",
-        keys.len(),
-        fmt_list(keys)
-    )?;
-    writeln!(
-        tables,
-        "static {case}CASE_TABLE_VALS: &[u32; {}] = &[{}];",
-        vals.len(),
-        fmt_list(vals)
-    )?;
-    write!(
-        tables,
-        "static {case}CASE_TABLE_MULTI: &[[char; 3]; {}] = &[{}];",
-        multis.len(),
-        fmt_list(multis)
-    )?;
+    writeln!(tables, "static {case}CASE_TABLES: &[Plane; {}] = &[", num_planes)?;
+    for plane in planes {
+        writeln!(tables, "    Plane {{")?;
+        for (name, table) in [
+            ("single_keys", &plane.single_keys[..]),
+            ("single_vals", &plane.single_vals[..]),
+            ("multi_keys", &plane.multi_keys[..]),
+        ] {
+            writeln!(tables, "        // {} entries, {} bytes", table.len(), size_of_val(table))?;
+            writeln!(tables, "        {name}: &[{}],", fmt_list(table))?;
+        }
+        writeln!(
+            tables,
+            "        // {} entries, {} bytes",
+            plane.multi_vals.len(),
+            size_of_val(&plane.multi_vals[..])
+        )?;
+        writeln!(tables, "        multi_vals: &[{}],", fmt_list(plane.multi_vals))?;
+        writeln!(tables, "    }},")?;
+    }
+    writeln!(tables, "];")?;
 
     Ok(tables)
 }
 
+#[derive(Copy, Clone)]
 struct CharEscape(char);
 
 impl fmt::Debug for CharEscape {
@@ -72,37 +91,62 @@ impl fmt::Debug for CharEscape {
     }
 }
 
+#[derive(Default, Clone)]
+struct HexEscape(u16);
+
+impl fmt::Debug for HexEscape {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:#06x?}", self.0)
+    }
+}
+
 static HEADER: &str = r"
-pub fn to_lower(c: char) -> [char; 3] {
-    if c.is_ascii() {
-        [(c as u8).to_ascii_lowercase() as char, '\0', '\0']
-    } else {
-        LOWERCASE_TABLE
-            .binary_search_by(|&(key, _)| key.cmp(&c))
-            .map(|i| {
-                let u = LOWERCASE_TABLE[i].1;
-                char::from_u32(u).map(|c| [c, '\0', '\0']).unwrap_or_else(|| {
-                    // SAFETY: Index comes from statically generated table
-                    unsafe { *multis.get_unchecked((u & (INDEX_MASK - 1)) as usize) }
-                })
-            }
-            Err(_) => [c, '\0', '\0'],
-        }
+struct Plane {
+    single_keys: &'static [u16],
+    single_vals: &'static [u16],
+    multi_keys: &'static [u16],
+    multi_vals: &'static [[char; 3]],
+}
+
+#[inline]
+fn lookup(c: char, tables: &[Plane]) -> [char; 3] {
+    let plane_index = (c as u32) / 0x10_000;
+    let code = ((c as u32) % 0x10_000) as u16;
+
+    let Some(plane) = tables.get(plane_index as usize) else {
+        return [c, '\0', '\0'];
+    };
+
+    if let Ok(multi_index) = plane.multi_keys.binary_search(&code) {
+        // SAFETY: Index comes from statically generated table
+        return *unsafe { plane.multi_vals.get_unchecked(multi_index) };
     }
 
+    if let Ok(single_index) = plane.single_keys.binary_search(&code) {
+        // SAFETY: Index comes from statically generated table
+        let code = *unsafe { plane.single_vals.get_unchecked(single_index) };
+        let c = unsafe { char::from_u32_unchecked((plane_index * 0x10_000) | (code as u32)) };
+        return [c, '\0', '\0'];
+    }
+
+    [c, '\0', '\0']
+}
+
+#[optimize(size)]
+pub fn to_lower(c: char) -> [char; 3] {
+    if c.is_ascii() {
+        return [c.to_ascii_lowercase(), '\0', '\0'];
+    }
+
+    lookup(c, LOWERCASE_TABLES)
+}
+
+#[optimize(size)]
 pub fn to_upper(c: char) -> [char; 3] {
     if c.is_ascii() {
-        [(c as u8).to_ascii_uppercase() as char, '\0', '\0']
-    } else {
-        UPPERCASE_TABLE
-            .binary_search_by(|&(key, _)| key.cmp(&c))
-            .map(|i| {
-                let u = UPPERCASE_TABLE[i].1;
-                char::from_u32(u).map(|c| [c, '\0', '\0']).unwrap_or_else(|| {
-                    // SAFETY: Index comes from statically generated table
-                    unsafe { *UPPERCASE_TABLE_MULTI.get_unchecked((u & (INDEX_MASK - 1)) as usize) }
-                })
-            })
-            .unwrap_or([c, '\0', '\0'])
+        return [c.to_ascii_uppercase(), '\0', '\0'];
     }
+
+    lookup(c, UPPERCASE_TABLES)
+}
 ";
